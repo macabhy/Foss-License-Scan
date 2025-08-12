7f9c2ffe5df2c2e3a79facaa31127277c9c56b17
macabhy <abhaykumar754@gmail.com>
2025-08-12 20:16:56 +0530

    Add dummy GPL-2.0 file for test

--- src/lodash/dummy-gpl-file.js
+++ src/lodash/dummy-gpl-file.js
@@ -1,3 +1,4 @@
-* This file is licensed under the GNU General Public License v2.0 only.  
- * SPDX-License-Identifier: GPL-2.0-only  
+/*
+ * This file is licensed under the GNU General Public License v2.0 only.
+ * SPDX-License-Identifier: GPL-2.0-only
  */
