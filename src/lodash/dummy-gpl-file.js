4a3da988f4d9fc306b10a3396b4bb7fcfc685e15
macabhy <abhaykumar754@gmail.com>
2025-08-21 12:35:04 +0530

    Add dummy GPL-2.0-only file for testing license scan

--- src/lodash/dummy-gpl-file.js
+++ src/lodash/dummy-gpl-file.js
@@ -1,17 +1,21 @@
 /*
- * Example Program - GPL-2.0-only
+ * This file is part of the Dummy Project.
  *
- * SPDX-License-Identifier: GPL-2.0-only
+ * Copyright (C) 2025 Abhay Kumar
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 2 only.
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
  */
+ 
+console.log("This is a dummy GPL-2.0-only file.");
